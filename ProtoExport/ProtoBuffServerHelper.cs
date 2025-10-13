3087742253b6399abdfe0bbd495cae91261dcd00
Blank <wangfj11@foxmail.com>
2025-10-13 17:03:59 +0800

    refactor(ProtoBuffServerHelper): 修改Post方法参数为LauncherOptions对象
    将Post方法的第二个参数从字符串路径改为LauncherOptions对象，提高代码的可维护性和扩展性

--- ProtoExport/ProtoBuffServerHelper.cs
+++ ProtoExport/ProtoBuffServerHelper.cs
@@ -151,7 +151,7 @@ namespace GameFrameX.ProtoExport
             File.WriteAllText(messageInfoList.OutputPath + ".cs", sb.ToString(), Encoding.UTF8);
         }
 
-        public void Post(List<MessageInfoList> operationCodeInfo, string launcherOptionsOutputPath)
+        public void Post(List<MessageInfoList> operationCodeInfo, LauncherOptions launcherOptions)
         {
         }
     }
